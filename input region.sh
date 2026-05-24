b5a49451274e6fef20ac64d73a2935eca88fa697
vchittimothu <saibaba,venkata86@gmail.com>
2026-05-24 16:05:24 +0530

    input region

--- input region.sh
+++ input region.sh
@@ -3,7 +3,7 @@ Region=$1
 echo $0
 echo $1
 echo "Lets get the VPCs information for the region $Region..."
-aws ecs describe-vpcs --region $Region | jq ".Vpcs[].VpcId" -r
-vpc_count=$(aws ecs describe-vpcs --region $Region | jq ".Vpcs[].VpcId" -r | wc -l)
+aws ec2 describe-vpcs --region $Region | jq ".Vpcs[].VpcId" -r
+vpc_count=$(aws ec2 describe-vpcs --region $Region | jq ".Vpcs[].VpcId" -r | wc -l)
 echo $vpc_count
 echo "$Region has total VPC counts of $vpc_count vpc...."
