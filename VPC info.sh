61f6b234df09789fc0a8ea719545cb41c3f8c416
vchittimothu <saibaba,venkata86@gmail.com>
2026-05-24 15:51:07 +0530

    VPC Info

--- VPC info.sh
+++ VPC info.sh
@@ -1,6 +1,6 @@
 #!bin/bash
-Echo "Lets get the VPCs information for Hyderabd region"
+echo "Lets get the VPCs information for Hyderabd region"
 aws ec2 describe-vpcs --region ap-south-2 | jq ".Vpcs[].VpcId" -r 
 Vpc_count=$(aws ec2 describe-vpcs --region ap-south-2 | jq ".Vpcs[].VpcId" -r | wc -l)
-Echo $Vpc_count
-Echo "ap-south-2 region has total $(Vpc_count) vpc"
+echo $Vpc_count
+echo "ap-south-2 region has total $(Vpc_count) vpc"
