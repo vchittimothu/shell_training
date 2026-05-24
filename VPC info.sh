ea115338aaaf25efe747ca1940c0878df7cbaa9e
vchittimothu <saibaba,venkata86@gmail.com>
2026-05-24 16:00:13 +0530

    input region

--- VPC info.sh
+++ VPC info.sh
@@ -1,6 +1,6 @@
 #!bin/bash
-echo "Lets get the VPCs information for Hyderabd region"
+egit cho "Lets get the VPCs information for Hyderabd region"
 aws ec2 describe-vpcs --region ap-south-2 | jq ".Vpcs[].VpcId" -r 
 Vpc_count=$(aws ec2 describe-vpcs --region ap-south-2 | jq ".Vpcs[].VpcId" -r | wc -l)
 echo $Vpc_count
-echo "ap-south-2 region has total $(Vpc_count) vpc"
+echo "ap-south-2 region has total $Vpc_count vpc"
---
+++ input region.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+Region=$1
+echo $0
+echo $1
+echo "Lets get the VPCs information for the region $Region..."
+aws ecs describe-vpcs --region $Region | jq ".Vpcs[].VpcId" -r | wc -l
+vpc_count=$(aws ecs describe-vpcs --region $Region | jq ".Vpcs[].VpcId" -r | wc -l)
+echo $vpc_count
+echo "$Region has total VPC counts of $vpc_count"
