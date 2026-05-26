c1af21ff825167852afd0832b011aca5d3c04d6f
vchittimothu <saibaba,venkata86@gmail.com>
2026-05-26 15:46:29 +0530

    multi args

--- multi positional args.sh
+++ multi positional args.sh
@@ -6,9 +6,17 @@ echo $0
 echo $1
 echo $2
 echo $3
-
-Echo "Lets get the VPC informations in $Region1"
+echo "Lets get the VPC informations in $Region1"
 aws ec2 describe-vpcs --region $Region1 | jq ".Vpcs[].VpcId" -r
 vpc_count=$(aws ec2 describe-vpcs --region $Region1 | jq ".Vpcs[].VpcId" -r|wc -l)
 echo " Total VPC count in $Region1 is $vpc_count"
 echo ##################################################################################
+echo "Lets get the VPC informations in $Region2"
+aws ec2 describe-vpcs --region $Region2 | jq ".Vpcs[].VpcId" -r
+vpc_count=$(aws ec2 describe-vpcs --region $Region2 | jq ".Vpcs[].VpcId" -r|wc -l)
+echo " Total VPC count in $Region2 is $vpc_count"
+echo ##################################################################################
+echo "Lets get the VPC informations in $Region3"
+aws ec2 describe-vpcs --region $Region3 | jq ".Vpcs[].VpcId" -r
+vpc_count=$(aws ec2 describe-vpcs --region $Region3 | jq ".Vpcs[].VpcId" -r|wc -l)
+echo " Total VPC count in $Region3 is $vpc_count"
