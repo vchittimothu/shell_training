dc6463a7498b1bb05228d7fc4a89beacfbc8d31a
vchittimothu <saibaba,venkata86@gmail.com>
2026-05-26 16:15:35 +0530

    if else

--- if else nginx.sh
+++ if else nginx.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 systemctl status nginx --no-pager > /dev/null
-if[$? -eq 0]
-then 
+if[ $? -eq 0 ];then 
 echo "Nginx service is running on the server and Lets validate using CURL...."
 curl -sl http://localhost | grep -i nginx
 else
