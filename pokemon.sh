c6219cdf02f227ce35e8540ca2c1a30cb104f6fd
vchittimothu <saibaba,venkata86@gmail.com>
2026-05-26 15:08:18 +0530

    Pokeapi

--- pokemon.sh
+++ pokemon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -sl https://pokeapi.co/api/v2/pokemon?limit=100000 | jq ".results[].name" -r
-curl -sl https://pokeapi.co/api/v2/pokemon?limit=100000 | jq ".results[].name" -r | wc-l
+curl -sl https://pokeapi.co/api/v2/pokemon?limit=100000 | jq ".results[].name" -r | wc -l
 
